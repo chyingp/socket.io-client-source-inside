cc23c7bb4d0bc2d6a53bca093309e7a8f0925158
casperchen(陈映平) <chyingp@gmail.com>
2018-03-17 18:24:03 +0800

    socket、manager之间的事件

---
+++ examples/eventBetweenManagerSocket.js
@@ -0,0 +1,8 @@
+/**
+ * socket 内部监听 manager 的事件，包括 open、packet、close
+ */
+const socket = require('socket.io-client')('http://localhost:3000');
+
+// socket.io.on('open');
+// socket.io.on('packet');
+// socket.io.on('close');
